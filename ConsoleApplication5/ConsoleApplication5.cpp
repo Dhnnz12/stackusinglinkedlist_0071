2ae0e22e1e2e65e45b13334a4ef2a9ec9f2275b3
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-10 13:34:00 +0700

    menambahkan fungsi pop

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -22,6 +22,14 @@ public:
 
 	int push(int value) {
 		Node* newNode = new Node();
-		newNode
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push value: " << value << endl;
+		return value;
+	}
+
+	void pop() {
+
 	}
 };
